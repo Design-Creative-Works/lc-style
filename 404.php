ebaa50e8deb37e7cc3b6949d16ceb70000274a55
designcreativite <73561739+designcreativite@users.noreply.github.com>
2021-10-14 19:58:29 +1100

    404 page update

---
+++ 404.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * 404 template.
+ *
+ * @package Neve
+ */
+
+get_header();
+// do_action( 'neve_do_404' );
+?>
+
+	<div id="primary" class="content-area" style="text-align:center">
+    <div class="container">
+		<div id="not-found">
+			<div class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title" >
+                        <?php 
+                        _e( 'Oops thats a 404!  well thats not good.....', 'neve' ); ?> </h1>              
+				</header><!-- .page-header -->
+				<div class="page-content" > 
+                     <h2>It looks like that page can't be found.</h2>   
+					<p>'Would you like to re-try your search?'</p>
+					<?php get_search_form(); ?>
+				</div><!-- .page-content -->
+			</div><!-- .error-404 -->
+        </div>
+		</div>
+        	
+    </div><!-- #primary -->
+
+<?php
+get_footer();
